ce8b15aa1662046429d48fe018c2d69b99569e58
srubino6 <46699139+srubino6@users.noreply.github.com>
2020-05-09 23:15:36 -0400

    Update main.js

--- hack/main.js
+++ hack/main.js
@@ -1,12 +1,3 @@
-// Your browser will call the onload() function when the document
-// has finished loading. In this case, onload() points to the
-// start() method we defined below. Because of something called
-// function hoisting, the start() method is callable on line 6
-// even though it is defined on line 8.
-// This is where all of our javascript code resides. This method
-// is called by "window" when the document (everything you see on
-// the screen) has finished loading.
-
 var colkeys = ["Healthy", "Sick", "Critical", "Recovered"];
     var color = d3.scaleOrdinal()
                 .domain(colkeys)
